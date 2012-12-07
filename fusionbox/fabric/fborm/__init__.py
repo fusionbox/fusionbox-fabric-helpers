88b8ff1b68fe812aeb69c395ebb621536ce4f319
Aaron Merriam <amerriam@fusionbox.com>
2012-12-07 12:01:32 -0700

    change fborm to use git pull to stage

--- fusionbox/fabric/fborm/__init__.py
+++ fusionbox/fabric/fborm/__init__.py
@@ -1,26 +1,20 @@
 from fabric.api import run, local, puts, cd, roles, env
 
-from fusionbox.fabric.git_helpers import get_git_branch, update_git_with_rsync, update_git_with_pull
+from fusionbox.fabric.git_helpers import get_git_branch, update_git_with_pull
 
 
 def get_fborm_folder():
-    """
-    Return the fborm directory.
-    """
     branch = get_git_branch()
     return '/var/www/fborm%s/' % ("" if branch == "master" else ":" + branch)
 
 
 @roles('dev')
 def correct():
-    """
-    Correct permissions on the dev server.
-    """
     run("sudo chgrp -R fusionbox /var/www/%s" % env.project_name)
     run("sudo chmod -R g+rwx /var/www/%s" % env.project_name)
-    run("sudo chmod o+w /var/www/%s/archive" % env.project_name)
-    run("sudo chmod o+w /var/www/%s/public_html/content" % env.project_name)
-    run("sudo chmod o+w /var/www/%s/public_html/img" % env.project_name)
+    #run("sudo chmod o+w /var/www/%s/archive" % env.project_name)
+    #run("sudo chmod o+w /var/www/%s/public_html/content" % env.project_name)
+    #run("sudo chmod o+w /var/www/%s/public_html/img" % env.project_name)
     run("sudo chmod -R g+rwx %s" % get_fborm_folder())
     run("sudo chmod o+w %s" % get_fborm_folder())
     run("rm -f /var/www/%s/.git/deploy_bundle" % env.project_name)
@@ -28,21 +22,17 @@ def correct():
 
 @roles('dev')
 def stage():
-    """
-    Stage will update the dev server with any changes.
-    """
-    local('git pull origin %s' % get_git_branch())
-    local('git push origin %s' % get_git_branch())
+    branch = get_git_branch()
+    local('git pull origin %s' % branch)
+    local('git push origin %s' % branch)
     with cd('/var/www/%s/' % env.project_name):
-        update_git_with_rsync(get_git_branch())
+        update_git_with_pull(branch)
         run("./fbmvc migrate latest")
+    correct()
 
 
 @roles('live')
 def deploy():
-    """
-    Deploy changes to the live server
-    """
     with cd('/var/www/%s/' % env.project_name):
         previous_head = update_git_with_pull('live')
         puts("Previous live HEAD: %s" % previous_head)
@@ -52,8 +42,5 @@ def deploy():
 
 @roles('live')
 def rollback(rev):
-    """
-    Rollback the live server to a specified commit.
-    """
     with cd('/var/www/%s/' % env.project_name):
         run("git checkout '%s'" % rev)
