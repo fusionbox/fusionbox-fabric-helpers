9be8e61d3be1129c8a12380ef4589b95657908c1
David Sanders <dsanders@fusionbox.com>
2012-12-14 11:49:28 -0700

    Update fborm fab routines

--- fusionbox/fabric/fborm/__init__.py
+++ fusionbox/fabric/fborm/__init__.py
@@ -4,43 +4,52 @@ from fusionbox.fabric import get_git_branch, get_update_function, fb_env
 
 
 def get_fborm_folder():
+    """
+    Gets the location of the fborm folder on the dev server.
+    """
     branch = get_git_branch()
-    return '/var/www/fborm%s/' % ("" if branch == "master" else ":" + branch)
+    return '/{0}/fborm{1}/'.format(
+        fb_env.dev_web_home,
+        '' if branch == 'master' else ':' + branch,
+    )
 
 
 def correct():
-    run("sudo chgrp -R fusionbox /var/www/%s" % fb_env.project_name)
-    run("sudo chmod -R g+rwx /var/www/%s" % fb_env.project_name)
-    #run("sudo chmod o+w /var/www/%s/archive" % fb_env.project_name)
-    #run("sudo chmod o+w /var/www/%s/public_html/content" % fb_env.project_name)
-    #run("sudo chmod o+w /var/www/%s/public_html/img" % fb_env.project_name)
-    run("sudo chmod -R g+rwx %s" % get_fborm_folder())
-    run("sudo chmod o+w %s" % get_fborm_folder())
+    """
+    Resets permissions on dev site files to avoid errors.
+
+    NOTE: This is a hack...there I said it.
+    """
+    run('sudo chgrp -R fusionbox /var/www/%s' % fb_env.dev_project_name)
+    run('sudo chmod -R g+rwx /var/www/%s' % fb_env.dev_project_name)
+    run('sudo chmod -R g+rwx %s' % get_fborm_folder())
+    run('sudo chmod o+w %s' % get_fborm_folder())
 
 
-def stage():
+def stage(branch=None, role='dev'):
+    """
+    Updates the remote site files to your local branch head and migrates.
+    """
     update_function = get_update_function()
-    branch = get_git_branch()
+    branch = branch or get_git_branch()
 
-    local('git pull origin %s' % branch)
-    local('git push origin %s' % branch)
+    project_loc = fb_env.role(role, 'project_loc')
 
-    with cd('/var/www/%s/' % fb_env.project_name):
-        update_function(branch)
-        run("./fbmvc migrate latest")
+    local('git pull origin {0}'.format(branch))
+    local('git push origin {0}'.format(branch))
 
-    correct()
+    with cd(project_loc):
+        previous_head = update_function(branch)
+        puts('Previous remote HEAD: {0}'.format(previous_head))
+        run('./fbmvc dbdump')
+        run('./fbmvc migrate latest')
+
+    if role == 'dev':
+        correct()
 
 
 def deploy():
-    update_function = get_update_function()
-
-    with cd('/var/www/%s/' % fb_env.project_name):
-        previous_head = update_function('live')
-        puts("Previous live HEAD: %s" % previous_head)
-        run("./fbmvc migrate latest")
-
-
-def rollback(rev):
-    with cd('/var/www/%s/' % fb_env.project_name):
-        run("git checkout '%s'" % rev)
+    """
+    Same as stage, but always uses the live branch and live config settings.
+    """
+    stage(branch='live', role='live')
