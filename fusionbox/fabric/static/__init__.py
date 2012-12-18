a3d9918c9d9471a40944642a53c3776bc44d3851
David Sanders <dsanders@fusionbox.com>
2012-12-18 09:41:38 -0700

    Add static deployment routines

---
+++ fusionbox/fabric/static/__init__.py
@@ -0,0 +1,34 @@
+from fabric.api import run, cd, puts
+
+from fusionbox.fabric import fb_env
+from fusionbox.fabric.git import get_git_branch
+from fusionbox.fabric.update import get_update_function
+
+
+def stage(branch=None, role='dev'):
+    """
+    Updates the remote site files to your local branch head.
+    """
+    update_function = get_update_function()
+    branch = branch or get_git_branch()
+
+    project_loc = fb_env.role(role, 'project_loc')
+
+    with cd(project_loc):
+        previous_head = update_function(branch)
+        puts('Previous remote HEAD: {0}'.format(previous_head))
+
+
+def deploy():
+    """
+    Same as stage, but always uses the live branch and live config settings.
+    """
+    stage(branch='live', role='live')
+
+
+def shell():
+    """
+    Fires up a shell on the live server.
+    """
+    with cd(fb_env.live_project_loc):
+        run('bash -')
