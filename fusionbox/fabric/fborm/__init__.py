0214090f3f07cf1a04606472b8abb140f1eb6590
David Sanders <dsanders@fusionbox.com>
2012-12-21 11:37:21 -0700

    Remove fborm permission correcting

--- fusionbox/fabric/fborm/__init__.py
+++ fusionbox/fabric/fborm/__init__.py
@@ -5,29 +5,6 @@ from fusionbox.fabric.git import get_git_branch
 from fusionbox.fabric.update import get_update_function
 
 
-def get_fborm_folder():
-    """
-    Gets the location of the fborm folder on the dev server.
-    """
-    branch = get_git_branch()
-    return '{0}/fborm{1}/'.format(
-        fb_env.dev_web_home,
-        '' if branch == 'master' else ':' + branch,
-    )
-
-
-def correct():
-    """
-    Resets permissions on dev site files to avoid errors.
-
-    NOTE: This is a hack...there I said it.
-    """
-    run('sudo chgrp -R fusionbox /var/www/%s' % fb_env.dev_project_dir)
-    run('sudo chmod -R g+rwx /var/www/%s' % fb_env.dev_project_dir)
-    run('sudo chmod -R g+rwx %s' % get_fborm_folder())
-    run('sudo chmod o+w %s' % get_fborm_folder())
-
-
 def stage(branch=None, role='dev'):
     """
     Updates the remote site files to your local branch head and migrates.
@@ -42,9 +19,6 @@ def stage(branch=None, role='dev'):
         puts('Previous remote HEAD: {0}'.format(previous_head))
         run('./fbmvc dbdump')
         run('./fbmvc migrate latest')
-
-    if role == 'dev':
-        correct()
 
 
 def deploy():
